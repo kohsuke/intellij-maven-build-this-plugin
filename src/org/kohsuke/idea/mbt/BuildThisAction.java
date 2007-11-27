f11fff755db6be7a6a4174e38bb8d414713c7b7e
kohsuke <null>
2007-11-27 18:24:19 +0000

    initial plugin

---
+++ src/org/kohsuke/idea/mbt/BuildThisAction.java
@@ -0,0 +1,61 @@
+package org.kohsuke.idea.mbt;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.idea.maven.builder.MavenBuilder;
+import org.jetbrains.idea.maven.builder.executor.MavenBuildParameters;
+import org.jetbrains.idea.maven.state.MavenProjectsState;
+
+import java.util.Arrays;
+
+/**
+ * Builds the current project.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class BuildThisAction extends AnAction {
+    public BuildThisAction() {
+    }
+
+    public void actionPerformed(AnActionEvent event) {
+        Project p = DataKeys.PROJECT.getData(event.getDataContext());
+
+        MavenBuildParameters params = new MavenBuildParameters();
+        params.setGoals(Arrays.asList("install"));
+
+        VirtualFile pom = findPom(event);
+
+        params.setPomPath(pom.getPath());
+
+        MavenProjectsState state = p.getComponent(MavenProjectsState.class);
+        params.setProfiles(state.getProfiles(pom));
+
+        MavenBuilder builder = p.getComponent(MavenBuilder.class);
+        builder.run(params);
+    }
+
+    public void update(AnActionEvent event) {
+        Presentation p = event.getPresentation();
+
+        p.setEnabled(findPom(event)!=null);
+    }
+    
+    private VirtualFile findPom(AnActionEvent event) {
+// getModuleFile() returned null.
+//        VirtualFile pom = m.getModuleFile().getParent().findChild("pom.xml");
+
+        VirtualFile file = DataKeys.VIRTUAL_FILE.getData(event.getDataContext());
+        return findPom(file);
+    }
+
+    private VirtualFile findPom(VirtualFile f) {
+        if(f==null) return null;
+        VirtualFile pom = f.findChild("pom.xml");
+        if(pom!=null)   return pom;
+        return findPom(f.getParent());
+    }
+}
