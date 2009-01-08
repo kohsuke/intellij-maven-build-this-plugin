92b1d7cad40b2f943cefdcd034aaf2fe5e09a896
kohsuke <null>
2009-01-08 17:23:13 +0000

    made to work with IDEA8

--- src/org/kohsuke/idea/mbt/BuildThisAction.java
+++ src/org/kohsuke/idea/mbt/BuildThisAction.java
@@ -1,19 +1,20 @@
 package org.kohsuke.idea.mbt;
 
+import com.intellij.execution.ExecutionException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.idea.maven.state.MavenProjectsState;
-import org.jetbrains.idea.maven.runner.executor.MavenRunnerParameters;
-import org.jetbrains.idea.maven.runner.MavenRunner;
+import org.jetbrains.idea.maven.project.MavenProjectsManager;
+import org.jetbrains.idea.maven.runner.MavenRunConfigurationType;
+import org.jetbrains.idea.maven.runner.MavenRunnerParameters;
 
-import java.util.Arrays;
+import java.util.Collections;
 
 /**
- * Builds the current project.
+ * Builds the current module.
  *
  * @author Kohsuke Kawaguchi
  */
@@ -23,24 +24,23 @@ public class BuildThisAction extends AnAction {
 
     public void actionPerformed(AnActionEvent event) {
         Project p = DataKeys.PROJECT.getData(event.getDataContext());
-
-        MavenRunnerParameters params = new MavenRunnerParameters();
-        params.setGoals(Arrays.asList("install"));
-
         VirtualFile pom = findPom(event);
 
-        params.setPomPath(pom.getPath());
-
-        MavenProjectsState state = p.getComponent(MavenProjectsState.class);
-        params.setProfiles(state.getProfiles(pom));
-
-        MavenRunner builder = p.getComponent(MavenRunner.class);
-        builder.run(params);
+        MavenRunnerParameters params = new MavenRunnerParameters(
+                true /*isPomExecution*/,
+                pom.getParent().getPath() /*working dir*/,
+                Collections.singletonList("install") /*goal*/,
+                MavenProjectsManager.getInstance(p).getActiveProfiles() /*profiles*/
+        );
+        try {
+            MavenRunConfigurationType.runConfiguration(p, params, event.getDataContext());
+        } catch (ExecutionException e) {
+            throw new Error(e); // don't know when this happens
+        }
     }
 
     public void update(AnActionEvent event) {
         Presentation p = event.getPresentation();
-
         p.setEnabled(findPom(event)!=null);
     }
     
